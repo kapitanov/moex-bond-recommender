e9fe44465a23b1a92f4a968dbe48c4a484a1bc41
Albert Kapitanov <albert-k@live.ru>
2021-10-02 16:41:50 +0300

    Фильтрация аномалий по доходности в коллекциях

--- pkg/recommender/collection_corporate.go
+++ pkg/recommender/collection_corporate.go
@@ -9,16 +9,28 @@ func init() {
 		// - нет признака "только для квалифицированных инвесторов"
 		// - нет признакак "высокий риск"
 		// - валюта номинала - рубль
+		// - приведенная доходность больше нуля и согласуется с критерием "три сигмы"
 		text := `
-SELECT bonds.id
-FROM bonds
-INNER JOIN issuers ON issuers.id = bonds.issuer_id
-WHERE is_traded
-  AND maturity_date IS NOT NULL
-  AND type = 'corporate_bond'
-  AND qualified_only = FALSE
-  AND high_risk = FALSE
-  AND face_unit = 'RUB'
+SELECT id
+FROM (
+         SELECT bonds.id,
+                r.interest_rate,
+                AVG(r.interest_rate) OVER ()    AS mean,
+                STDDEV(r.interest_rate) OVER () AS stddev
+         FROM bonds
+                  INNER JOIN issuers
+                             ON issuers.id = bonds.issuer_id
+                  INNER JOIN reports r ON bonds.id = r.bond_id
+
+         WHERE is_traded
+           AND maturity_date IS NOT NULL
+           AND type = 'corporate_bond'
+           AND qualified_only = FALSE
+           AND high_risk = FALSE
+           AND face_unit = 'RUB'
+           AND r.interest_rate > 0
+     ) xs
+WHERE interest_rate <= (mean + 3 * stddev)
 `
 		return text
 	})
